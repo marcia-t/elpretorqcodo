3b99930f598401a206cc2f16afa24d999b258e7e
tejedamarcia@gmail.com <tejedamarcia@gmail.com@af11d535-1d17-bc21-a4f6-c55ebf3f2ecc>
2012-10-28 14:46:24 +0000

    Cambios en el menú!

--- elpretorQcodo/includes/header.inc.php
+++ elpretorQcodo/includes/header.inc.php
@@ -28,22 +28,43 @@
          
          <li><a href='http://qcodo/drafts/buscar_tramites.php'><span>Buscar trámite</span></a></li>
          <li><a href='http://qcodo/drafts/tramites_asignados_edit.php'><span>Asignar nuevo trámite</span></a></li>
-         <li><a href='http://qcodo/drafts/tipo_tramites_list.php'><span>Ver tipos de trámite</span></a></li>
-         <li><a href='http://qcodo/drafts/tipo_tramites_edit.php'><span>Crear un nuevo tipo de trámite</span></a></li>
          
+         <li><a href='http://qcodo/drafts/carpeta_tramites.php'><span>Carpeta de trámites</span></a></li>
       </ul>
    </li>
    <li class='has-sub'><a href='#'><span>Sistema</span></a>
  	  <ul>
-         <li><a href='http://qcodo/drafts/agentes_list.php'><span>Ver agentes</span></a></li>
-         <li><a href='http://qcodo/drafts/agentes_edit.php'><span>Agregar nuevo agente</span></a></li>
-         <li><a href='http://qcodo/drafts/zonas_list.php'><span>Ver zonas</span></a></li>
-         <li><a href='http://qcodo/drafts/zonas_edit.php'><span>Agregar una nueva zona</span></a></li>
-         <li><a href='http://qcodo/drafts/abogados_list.php'><span>Ver abogados</span></a></li>
-         <li><a href='http://qcodo/drafts/abogados_edit.php'><span>Agregar un nuevo abogado</span></a></li>
-         <li><a href='http://qcodo/drafts/modalidades_list.php'><span>Ver modalidades</span></a></li>
-         <li><a href='http://qcodo/drafts/modalidades_edit.php'><span>Agregar una nueva modalidad</span></a></li>
-          <li><a href='http://qcodo/drafts/carpeta_tramites.php'><span>Carpeta de trámites</span></a></li>
+ 	  	<li class='has-sub'><a href='#'><span>Agentes</span></a>
+ 	  	<ul>
+         <li><a href='http://qcodo/drafts/agentes_list.php'><span>Ver</span></a></li>
+         <li><a href='http://qcodo/drafts/agentes_edit.php'><span>Agregar</span></a></li>
+         </ul>
+         </li>
+         <li class='has-sub'><a href='#'><span>Zonas</span></a>
+ 	  	<ul>
+         <li><a href='http://qcodo/drafts/zonas_list.php'><span>Ver</span></a></li>
+         <li><a href='http://qcodo/drafts/zonas_edit.php'><span>Agregar</span></a></li>
+         </ul>
+         </li>
+         <li class='has-sub'><a href='#'><span>Abogados</span></a>
+ 	  	<ul>
+         <li><a href='http://qcodo/drafts/abogados_list.php'><span>Ver</span></a></li>
+         <li><a href='http://qcodo/drafts/abogados_edit.php'><span>Agregar</span></a></li>
+         </ul>
+         </li>
+         <li class='has-sub'><a href='#'><span>Modalidades</span></a>
+ 	  	<ul>
+         <li><a href='http://qcodo/drafts/modalidades_list.php'><span>Ver</span></a></li>
+         <li><a href='http://qcodo/drafts/modalidades_edit.php'><span>Agregar</span></a></li>
+         </ul>
+         </li>
+         <li class='has-sub'><a href='#'><span>Tipo de trámites</span></a>
+ 	  	<ul>
+         <li><a href='http://qcodo/drafts/tipo_tramites_list.php'><span>Ver</span></a></li>
+         <li><a href='http://qcodo/drafts/tipo_tramites_edit.php'><span>Agregar</span></a></li>
+         </ul>
+         </li>
+          
       </ul>
        <li class='has-sub'><a href='#'><span>Caja</span></a> </li>
        <li class='has-sub'><a href='#'><span>Declaratoria</span></a> </li>
--- elpretorQcodo/www/drafts/index.php
+++ elpretorQcodo/www/drafts/index.php
@@ -1,57 +1,187 @@
 <?php
-	// Include prepend.inc to load Qcodo
-	require(dirname(__FILE__) . '/../../includes/prepend.inc.php');
+require(dirname(__FILE__) . '/../../includes/prepend.inc.php');
 
-	// Security check for ALLOW_REMOTE_ADMIN
-	// To allow access REGARDLESS of ALLOW_REMOTE_ADMIN, simply remove the line below
-	QApplication::CheckRemoteAdmin();
-
-	// Iterate through the files in this "form_drafts" folder, looking for files
-	// that end in _edit.php or _list.php
-	$strSuffixes = array('_edit.php', '_list.php');
-	$strObjectArray = array();
-	$objDirectory = opendir(dirname(__FILE__));
-	while ($strFilename = readdir($objDirectory)) {
-		if (($strFilename != '.') && ($strFilename != '..')) {
-			$blnFound = false;
-			// strip the suffix (if applicable)
-			foreach ($strSuffixes as $strSuffix) {
-				if ((!$blnFound) && 
-					(substr($strFilename, strlen($strFilename) - strlen($strSuffix)) == $strSuffix)) {
-					$strFilename = substr($strFilename, 0, strlen($strFilename) - strlen($strSuffix));
-					$blnFound = true;
-				}
-			}
-
-			if ($blnFound)
-				$strObjectArray[$strFilename] = true;
-		}
+class CarpetaTramitesForm extends QForm {
+	
+	protected $lstAgentes;
+	protected $lstZonas;
+	protected $pnlList;
+	protected $pnlEdit;
+	protected $mctTramitesAsignados;
+	protected $mctTipoTramite;
+	protected $btnMostrar;
+	protected $dtgTramitesAsignadoses;
+	protected $idAgente;
+	protected $idZona;
+	protected $lblResponse;
+	protected $chkFinalizados;
+	
+	
+	protected function Form_Create() {
+		
+		$this->chkFinalizados = new QCheckBox($this);
+		$this->chkFinalizados->Checked = false;
+		//$this->chkFinalizados->AddAction(new QClickEvent(), new QServerAction('chkSelected_Click'));
+		
+		$this->lblResponse = new QLabel($this);
+		$this->lblResponse->HtmlEntities = false;
+		
+		$this->mctTramitesAsignados = TramitesAsignadosMetaControl::CreateFromPathInfo($this);
+		$this->mctTipoTramite = TipoTramitesMetaControl::CreateFromPathInfo($this);
+		$this->pnlList = new QPanel($this, 'pnlList');
+		$this->pnlList->AutoRenderChildren = true;
+		
+		$this->pnlEdit = new QPanel($this, 'pnlEdit');
+		$this->pnlEdit->AutoRenderChildren = true;
+		$this->pnlEdit->Visible = false;
+		
+		
+		$this->dtgTramitesAsignadoses = new TramitesAsignadosDataGrid($this);
+		$this->dtgTramitesAsignadoses->CssClass = 'datagrid';
+		$this->dtgTramitesAsignadoses->AlternateRowStyle->CssClass = 'alternate';
+		
+		
+		
+		// Add Pagination (if desired)
+		$this->dtgTramitesAsignadoses->Paginator = new QPaginator($this->dtgTramitesAsignadoses);
+		$this->dtgTramitesAsignadoses->ItemsPerPage = 20;
+		
+		//$this->dtgTramitesAsignadoses->SetDataBinder('bindBySearch');
+		
+		
+		//$strEditPageUrl = __VIRTUAL_DIRECTORY__ . __FORM_DRAFTS__ . '/tramites_asignados_edit.php';
+		//$this->dtgTramitesAsignadoses->MetaAddEditLinkColumn($strEditPageUrl, 'Edit', 'Edit');
+		
+		$this->dtgTramitesAsignadoses->AgregarColumna('IdTramiteAsignado', 'Trámite');
+			$this->dtgTramitesAsignadoses->AgregarColumna(QQN::TramitesAsignados()->IdAgenteObject, 'Agente');
+			$this->dtgTramitesAsignadoses->AgregarColumna(QQN::TramitesAsignados()->NroAbogadoObject, 'Abogado');
+			$this->dtgTramitesAsignadoses->AgregarColumna(QQN::TramitesAsignados()->IdEstadoObject, 'Estado');
+			$this->dtgTramitesAsignadoses->MetaAddColumn('FechaIngreso');
+			$this->dtgTramitesAsignadoses->MetaAddColumn('FechaSalida');
+			$this->dtgTramitesAsignadoses->AgregarColumna(QQN::TramitesAsignados()->TipoTramiteObject, 'Tipo de trámite');
+			$this->dtgTramitesAsignadoses->MetaAddColumn('FechaVencimiento');
+		
+		$this->dtgTramitesAsignadoses->AddColumn(new QDataGridColumn('En curso', '<?= $_FORM->encurso_Render($_ITEM) ?>','HtmlEntities=false'));
+		$this->dtgTramitesAsignadoses->AddColumn(new QDataGridColumn('De regreso', '<?= $_FORM->deregreso_Render($_ITEM) ?>','HtmlEntities=false'));
+		$this->dtgTramitesAsignadoses->AddColumn(new QDataGridColumn('Finalizar', '<?= $_FORM->finalizar_Render($_ITEM) ?>','HtmlEntities=false'));
+		$this->dtgTramitesAsignadoses->DataSource = TramitesAsignados::LoadAll();
+		
+		$this->lstAgentes = $this->mctTramitesAsignados->lstIdAgenteObject_Create();
+		$this->lstZonas = $this->mctTipoTramite->lstIdZonaObject_Create();
+		
+		$this->btnMostrar = new QButton($this);
+		$this->btnMostrar->Text = 'Mostrar';
+		$this->btnMostrar->AddAction(new QClickEvent(), new QAjaxAction('btnMostrar_Click'));
 	}
-
-	// Sort the list of objects
-	ksort($strObjectArray);
-
-	$strPageTitle = QApplication::Translate('List of Form Drafts');
-	require(__INCLUDES__ . '/header.inc.php');
-?>
-
-	<div id="titleBar">
-		<h2 id="right"><a href="<?php _p(__VIRTUAL_DIRECTORY__ . __PANEL_DRAFTS__) ?>/index.php">&laquo; <?php _t('Go to "Panel Drafts"'); ?></a></h2>
-		<h2><?php _t('Form Drafts') ?></h2>
-		<h1><?php _t('List of Form Drafts') ?></h1>
-	</div>
-
-	<div id="draftList">
-<?php
-		foreach ($strObjectArray as $strObject=>$blnValue) {
-			printf('<h1>%s</h1><p class="create"><a href="%s/%s_list.php">%s</a> &nbsp;|&nbsp; <a href="%s/%s_edit.php">%s</a></p>',
-				$strObject, __VIRTUAL_DIRECTORY__ . __FORM_DRAFTS__, $strObject, QApplication::Translate('View List'),
-				__VIRTUAL_DIRECTORY__ . __FORM_DRAFTS__, $strObject, QApplication::Translate('Create New'));
+	
+	
+	public function finalizar_Render(TramitesAsignados $objTramiteAsignado) {
+		$strControlId = 'chkSelected' . $objTramiteAsignado->IdTramiteAsignado;
+	
+		$chkSelected = $this->GetControl($strControlId);
+	
+		if (!$chkSelected) {
+			$chkSelected = new QLinkButton($this->dtgTramitesAsignadoses);
+			$chkSelected->Text = 'Finalizar';
+	
+			$chkSelected->ActionParameter = $objTramiteAsignado->IdTramiteAsignado;
+				
+				
+			$chkSelected->AddAction(new QClickEvent(), new QServerAction('finalizar_Click'));
 		}
-?>
-		<p>&nbsp;</p>
-		<h1><?php _t('Panel Drafts') ?></h1>
-		<p class="create"><a href="<?php _p(__VIRTUAL_DIRECTORY__ . __PANEL_DRAFTS__) ?>"><?php _t('Go to "Panel Drafts"') ?></a></p>
-	</div>
-
-<?php require (__INCLUDES__ . '/footer.inc.php'); ?>
+	
+		return $chkSelected->Render(false);
+	}
+	
+	protected function finalizar_Click($strFormId, $strControlId, $strParameter) {
+		$intTramiteAsignado = $strParameter;
+		TramitesAsignados::UpdateEstadoFinalizado($intTramiteAsignado);
+		$this->dtgTramitesAsignadoses->DataSource  = $this->generaSQLYBuscar();
+		
+	}
+	
+	
+	public function encurso_Render(TramitesAsignados $objTramiteAsignado) {
+		$strControlId = 'chkSelected' . $objTramiteAsignado->IdTramiteAsignado;
+	
+		$chkSelected = $this->GetControl($strControlId);
+	
+		if (!$chkSelected) {
+			$chkSelected = new QLinkButton($this->dtgTramitesAsignadoses);
+			$chkSelected->Text = 'En curso';
+	
+			$chkSelected->ActionParameter = $objTramiteAsignado->IdTramiteAsignado;
+	
+	
+			$chkSelected->AddAction(new QClickEvent(), new QServerAction('encurso_Click'));
+		}
+	
+		return $chkSelected->Render(false);
+	}
+	
+	protected function encurso_Click($strFormId, $strControlId, $strParameter) {
+		$intTramiteAsignado = $strParameter;
+		TramitesAsignados::UpdateEstadoEnCurso($intTramiteAsignado);
+		$this->dtgTramitesAsignadoses->DataSource  = $this->generaSQLYBuscar();
+	}
+	
+	
+	
+	
+	public function deregreso_Render(TramitesAsignados $objTramiteAsignado) {
+		$strControlId = 'chkSelected' . $objTramiteAsignado->IdTramiteAsignado;
+	
+		$chkSelected = $this->GetControl($strControlId);
+	
+		if (!$chkSelected) {
+			$chkSelected = new QLinkButton($this->dtgTramitesAsignadoses);
+			$chkSelected->Text = 'De regreso';
+	
+			$chkSelected->ActionParameter = $objTramiteAsignado->IdTramiteAsignado;
+	
+	
+			$chkSelected->AddAction(new QClickEvent(), new QServerAction('deregreso_Click'));
+		}
+	
+		return $chkSelected->Render(false);
+	}
+	
+	protected function deregreso_Click($strFormId, $strControlId, $strParameter) {
+		$intTramiteAsignado = $strParameter;
+		TramitesAsignados::UpdateEstadoDeRegreso($intTramiteAsignado);
+		$this->dtgTramitesAsignadoses->DataSource  = $this->generaSQLYBuscar();
+	}
+	
+	
+	protected function btnMostrar_Click($strFormId, $strControlId, $strParameter) {
+	
+		$this->idAgente = $this->lstAgentes->SelectedValue;
+		$this->idZona = $this->lstZonas->SelectedValue;
+		$this->dtgTramitesAsignadoses->DataSource = $this->generaSQLYBuscar();
+		
+	}
+	
+	public function generaSQLYBuscar(){
+	
+		$objDatabase = TramitesAsignados::GetDatabase();
+	
+		$sql = "SELECT * FROM sistema.tramites_asignados
+				LEFT JOIN sistema.tipo_tramites 
+				ON tramites_asignados.tipo_tramite = tipo_tramites.id_tipo_tramite
+				WHERE 1 = 1";
+		if (!$this->chkFinalizados->Checked){
+			$sql.= " AND id_estado != 4";
+		}
+		
+		if ($this->idAgente)
+			$sql.= " AND id_agente = $this->idAgente";
+		
+		if ($this->idZona)
+			$sql.= " AND id_zona = $this->idZona";
+		
+		$objDbResult = $objDatabase->Query($sql);
+		return TramitesAsignados::InstantiateDbResult($objDbResult);
+	}
+	
+}
+CarpetaTramitesForm::Run('CarpetaTramitesForm');
